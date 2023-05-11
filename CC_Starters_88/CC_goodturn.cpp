93b97ec547dc3fc61b15dc67bf812bfb3014acd5
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2023-05-11 17:32:04 +0530

    cc

---
+++ CC_Starters_88/CC_goodturn.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,a,b;
+    cin>>t;
+    while(t-->0)
+    {
+       cin>>a>>b;
+       if(a+b>6)
+       cout<<"YES"<<endl;
+       else
+       cout<<"NO"<<endl;
+    }
+    
+    
+	// your code goes here
+	return 0;
+}
