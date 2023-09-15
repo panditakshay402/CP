987e40f25d47a27aed88f97e15c232fedc32c811
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2023-09-15 18:01:15 +0530

    addFunction

---
+++ CC_Starters_88/CPP/fn1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int summ(int a, int b)
+{
+ int sum  = a+b;
+ return sum;
+}
+
+int  main()
+{
+    int a,b;
+    cin>>a>>b;
+   int res=summ(a,b);
+cout<<res;
+
+return 0;
+}
+
--- CC_Starters_88/CPP/usingNamespaceStd.cpp
+++ CC_Starters_88/CPP/usingNamespaceStd.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
 int main()
-{
+{   int a;
     cout<<"Akshay \n"<<"next line";
-
+    cin>>a;cout<<"Akshay \n"<<"next line \n"<<a;
     return 0;
 
 }
