5875909fce34b8470fae23fdb8171050dc8c85e0
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2023-09-16 20:25:48 +0530

    half triangle

---
+++ CC_Starters_88/CPP/InbuiltMAxMinFunction.cpp
@@ -0,0 +1,12 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b; 
+    cin>>a>>b;
+    int c= min (a,b);
+    cout<<c;
+
+    
+}
---
+++ CC_Starters_88/CPP/pattern.cpp
@@ -0,0 +1,26 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+int main()
+{
+
+for(int i=0; i<5; i++)
+{
+    for(int j=0; j<5; j++)
+    {
+        cout<<"*";
+    }
+    cout<<"\n";
+}
+
+
+
+void printTriangle(int n) {
+        for(int i=0; i<n; i++)
+        { for(int j=0; j<=i; j++){
+            cout<<"*"<<" ";
+            
+        }cout<<"\n";
+        }
+    }
+}
